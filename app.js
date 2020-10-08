7551d00814eb478760373f689ed5d5f822d9ccf1
mitty009 <mitchell.thomas808@gmail.com>
2020-10-08 18:58:06 +0300

    Stable 10x10 grid with 100 array

--- app.js
+++ app.js
@@ -1,33 +1,75 @@
 const grid = document.querySelector('.grid')
-const width = 5
+const width = 10
 const cells = []
-const playerChoice = []
-const availableChoice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+const battleShip1 = []
+const battleShip2 = []
+const battleShip3 = []
+const availableChoice = []
+
+for (let i = 1; i < width ** 2 + 1; i++) {
+  availableChoice.push(i)
+}
+
+console.log(availableChoice)
 
 // Create grid pattern
 for (let i = 0; i < width ** 2; i++) {
   const div = document.createElement('div')
   div.classList.add('cells')
+  div.classList.add('available')
   div.setAttribute('id', i + 1)
   grid.appendChild(div)
   cells.push(div)
 }
 
-function playerChooses(){
+let cellNumber = 0
+// Set player choices and remove available choices
+function playerChooses() {
   cells.forEach((cell) => {
     cell.addEventListener('click', () => {
-      if (playerChoice.length < 12) {
-        playerChoice.push(Number(cell.id))
-        console.log(playerChoice)
-        for (let i = 0; i < availableChoice.length; i++) {
-          if (availableChoice[i] === Number(cell.id)) {
-            availableChoice.splice(i, 1)
-            console.log(availableChoice)
-            console.log(playerChoice)
-          }
-        }
+      cellNumber = Number(cell.id)
+
+      if (battleShip1.length < 3) {
+        battleShip1.push(cellNumber)
+        cell.classList.add('selected')
+        console.log('battleship 1 ' + battleShip1)
+        removeChoices()
+        console.log(availableChoice)
+
+      } else if (battleShip2.length < 4) {
+        battleShip2.push(cellNumber)
+        cell.classList.add('selected')
+        console.log('battleship 2 ' + battleShip2)
+        removeChoices()
+
+
+      } else if (battleShip3.length < 5) {
+        battleShip3.push(cellNumber)
+        cell.classList.add('selected')
+        console.log('battleship 3 ' + battleShip3)
+        removeChoices()
+
+      } else {
+        console.log('All battleship positions taken')
       }
     })
   })
 }
-// Set player choices and remove available choices
+
+function removeChoices() {
+  for (let i = 0; i < availableChoice.length; i++) {
+    if (availableChoice[i] === cellNumber) {
+      availableChoice.splice(i, 1)   
+    }
+  }
+}
+
+function checkDiagonal() {
+
+}
+
+function checkLocation() {
+
+}
+
+playerChooses()
